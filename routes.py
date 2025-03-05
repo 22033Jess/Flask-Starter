55a6bb0adb05cceb42e27ae1f5eeb9c69f030e2b
22033Jess <166669401+22033Jess@users.noreply.github.com>
2025-03-05 20:25:33 +1300

    first commit

---
+++ routes.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template
+import sqlite3
+
+# create the Flask app
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html', title='HOME')
+
+
+@app.route('/me')
+def me():
+    title = 'ME'  # an alternative way to send a variable to the template
+    return render_template('me.html', title=title)
+
+
+@app.route('/pet_rocks/<int:id>')  # only accept an INT in this position
+def pet_rocks(id):  # must have the variable (id) in the function signature too
+    id = id * 2  # just to show you can process the variable if you need to
+    return render_template('pet_rocks.html', title='PET ROCKS', id=id)
+
+
+@app.route('/adder/<int:one>/<int:two>')
+def adder(one, two):
+    total = one + two
+    # what follows is an alternative way to structure the render_template
+    # to ensure you don't breach the 79 character limit with lots of
+    # parameters in the 'render_template' function call
+    return render_template('adder.html', one=one, two=two, total=total, title=total)
+
+
+@app.route('/all_pizzas')
+def all_pizzas():
+    conn = sqlite3.connect('pizza.db') # connect to the database
+    cur = conn.cursor() # create a cursor to read lines from the database
+    cur.execute('SELECT * FROM pizza') # give cursor a job to do
+    pizzas = cur.fetchall()   # fetch all the results
+    conn.close() # close the connection (you opened it so close it)
+    return render_template('all_pizzas.html', pizzas=pizzas)
+
+
+@app.route('/pizza/<int:id>')
+def pizza(id):
+    conn = sqlite3.connect('pizza.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM pizza WHERE id = ?', (id,))
+    pizza = cur.fetchone()
+    cur.execute('SELECT Topping.name, Topping.description, Topping.gf, Topping.vegan FROM PizzaTopping JOIN Topping ON PizzaTopping.tid = Topping.id WHERE PizzaTopping.pid = ?', (id,))
+    pizza_toppings = cur.fetchall()
+    conn.close()
+
+    return render_template('pizza.html', pizza=pizza, pizza_toppings=pizza_toppings)
+
+if __name__ == '__main__':
+    app.run(debug=True)
